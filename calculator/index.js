44f08c886eb9257265a40e152752562693094f00
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-06 14:22:54 +0200

    calculator with TS:: by sergio giraldo @ 20220806T1422CEST, gpg signed

---
+++ calculator/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Benefits of TypeScript
+// challenges:
+// - give the screen's innerHTML a string
+// - change the two input values to numbers
+exports.__esModule = true;
+exports.screen = void 0;
+var button = document.querySelector('.button');
+var firstInput = document.querySelector('#first-input');
+var secondInput = document.querySelector('#second-input');
+exports.screen = document.querySelector('.screen');
+function addNumbers(a, b) {
+    exports.screen.innerHTML = (a + b).toString();
+}
+button.addEventListener('click', function () {
+    return addNumbers(parseInt(firstInput.value), parseInt(secondInput.value));
+});
